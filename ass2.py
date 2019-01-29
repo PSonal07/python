2da7e65f37245f16eefc2d112ec813273e3ec1f6
Priyanka Sanjay Raut <PRaut@slb.com>
2019-01-29 11:17:29 +0530

    Initial draft

---
+++ ass2.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 28 14:56:15 2019
+
+@author: PRaut
+"""
+
+first_no = int(input("Enter an first no. :"))
+second_no = int(input("Enter an second no. :"))
+
+print(first_no, second_no)
+
+print(first_no+second_no)
+total=sum([first_no,second_no])
+print("Sum",total)
+
